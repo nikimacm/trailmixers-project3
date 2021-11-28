44e26076dd194e1c8a65facd98d17350f2181b02
nikimacm <nickymacm@gmail.com>
2021-11-28 17:06:12 +0000

    added function to allow users to add new trails

--- trails.py
+++ trails.py
@@ -5,7 +5,7 @@ if os.path.exists("env.py"):
 
 
 MONGO_URI = os.environ.get("MONGO_URI")
-DATABASE = "TrailMixers"
+DATABASE = "TrailMixer"
 COLLECTION = "trails"
 
 
@@ -29,11 +29,62 @@ def show_menu():
     return option
 
 
+def get_record():
+    print("")
+    first = input("Enter first name > ")
+    last = input("Enter last name > ")
+
+    try:
+        doc = coll.find_one({"first": first.lower(), "last": last.lower()})
+    except:
+        print("Error accessing the database")
+
+    if not doc:
+        print("")
+        print("Error! No results found.")
+
+    return doc
+
+
+def add_record():
+    print("")
+    title = input("Enter trail title > ")
+    address = input("Enter address > ")
+    description = input("Enter description > ")
+    difficulty = input("Enter difficulty > ")
+    directions = input("Enter directions > ")
+    elevation = input("Enter elevation > ")
+    image = input("Upload image > ")
+    length = input("Enter length > ")
+    time = input("Enter time taken > ")
+    type = input("Enter type of trail > ")
+
+    new_doc = {
+        "title": trail.title(),
+        "address": trail.address(),
+        "description": description,
+        "difficulty": difficulty,
+        "directions": directions,
+        "elevation": elevation,
+        "image": image,
+        "length": length
+        "time": time,
+        "type": type,
+    }
+
+    try:
+        coll.insert(new_doc)
+        print("")
+        print("Document inserted")
+    except:
+        print("Error accessing the database")
+
+
 def main_loop():
     while True:
         option = show_menu()
         if option == "1":
-            print("You have selected option 1")
+            add_record()
         elif option == "2":
             print("You have selected option 2")
         elif option == "3":
