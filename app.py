c33c31b2ad8b07a8194fe7ad699ccc999bd21f41
nikimacm <nickymacm@gmail.com>
2021-11-28 19:39:08 +0000

    recreated the flask app, have started from scratch due to too many errors

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 import os
-from flask import (
-    Flask, flash, render_template,
-    redirect, request, session, url_for)
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
+from flask import Flask
 if os.path.exists("env.py"):
     import env
 
@@ -11,40 +7,9 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 
 
-app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.secret_key = os.environ.get("SECRET_KEY")
-
-
-mongo = PyMongo(app)
-
-
 @app.route("/")
-@app.route("/get_comments")
-def get_comments():
-    comments = mongo.db.comments.find()
-    return render_template("comments.html", comments=comments)
-
-
-@app.route("/")
-@app.route("/get_ratings")
-def get_ratings():
-    ratings = mongo.db.ratings.find()
-    return render_template("ratings.html", ratings=ratings)
-
-
-@app.route("/")
-@app.route("/get_trails")
-def get_trails():
-    trails = mongo.db.trails.find()
-    return render_template("trails.html", trails=trails)
-
-
-@app.route("/")
-@app.route("/get_users")
-def get_users():
-    users = mongo.db.users.find()
-    return render_template("users.html", users=users)
+def hello():
+    return "Hello World ... again!"
 
 
 if __name__ == "__main__":
