0ff43b0d01d6948e5c527618b203dcb2847d0f3f
nikimacm <nickymacm@gmail.com>
2021-11-30 18:25:18 +0000

    changed debug to false as cannot connect app through heroku, so testing

--- app.py
+++ app.py
@@ -106,4 +106,4 @@ def add_trail():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
